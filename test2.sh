fc8cd1343bee38e2dac27d22ce14043e4c9d1bdb
BonniCapron <aserbak87@gmail.com>
2025-09-29 08:56:51 +0300

    Delete test2.sh

--- test2.sh
+++
@@ -1,9 +0,0 @@
-read -p "Введіть число: " number
-if [ "$number" -gt 0 ]; then
-    echo "Число $number є додатнє"
-elif [ "$number" -lt 0 ]; then
-    echo "Число $number є від’ємне"
-else
-    echo "Число $number дорівнює нулю"
-fi
-
